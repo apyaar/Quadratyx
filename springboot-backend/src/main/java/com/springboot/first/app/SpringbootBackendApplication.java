d26b081916b57693d4242d7d99f5acc79451f2cf
Ayushi Pal <111197057+apyaar@users.noreply.github.com>
2023-02-13 16:55:18 +0530

    Add files via upload

---
+++ springboot-backend/src/main/java/com/springboot/first/app/SpringbootBackendApplication.java
@@ -0,0 +1,19 @@
+package com.springboot.first.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.springboot.first.model.Employee;
+
+@SpringBootApplication
+public class SpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendApplication.class, args);
+		
+	}
+	
+	
+
+}
